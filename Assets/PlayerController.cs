07d118b0c10f5f0f0ebe92a371765110d675f650
merLO <ilia.reingold@gmail.com>
2020-10-21 17:18:37 -0400

    removes broken animations, reduces diagonal speed

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+#pragma warning disable CS0618 // Type or member is obsolete
 public class PlayerController : NetworkBehaviour
+#pragma warning restore CS0618 // Type or member is obsolete
 {
-  Rigidbody2D body;
+  Rigidbody2D rb;
   Animator animator;
 
   float horizontal;
@@ -17,19 +19,10 @@ public class PlayerController : NetworkBehaviour
   void Start()
   {
     animator = GetComponent<Animator>();
-    body = GetComponent<Rigidbody2D>();
+    rb = GetComponent<Rigidbody2D>();
 
-    if (!isLocalPlayer)
-    {
-      localPlayerSprite = Resources.Load<Sprite>("mage-black");
-      GetComponent<SpriteRenderer>().sprite = localPlayerSprite;
-    }
-    else
-    {
-      localPlayerSprite = Resources.Load<Sprite>("mage-white");
-
-      GetComponent<SpriteRenderer>().sprite = localPlayerSprite;
-    }
+    localPlayerSprite = Resources.Load<Sprite>("mage-black");
+    GetComponent<SpriteRenderer>().sprite = localPlayerSprite;
   }
 
   void Update()
@@ -40,20 +33,25 @@ public class PlayerController : NetworkBehaviour
 
   private void FixedUpdate()
   {
-    animator.SetFloat("directionX", horizontal);
-    animator.SetFloat("directionY", vertical);
-
-    if (horizontal == 0 && vertical == 0)
+    if (horizontal > 0)
     {
-      animator.enabled = false;
-    }
-    else
-    {
-      animator.enabled = true;
+      transform.rotation = Quaternion.Euler(transform.rotation.x, 180f, transform.rotation.z);
     }
 
-    if (!isLocalPlayer) return;
+    if (horizontal < 0)
+    {
+      transform.rotation = Quaternion.Euler(transform.rotation.x, 0f, transform.rotation.z);
+    }
 
-    body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    if (isLocalPlayer)
+    {
+      if (horizontal != 0 && vertical != 0)
+      {
+        rb.velocity = new Vector2(horizontal * (runSpeed * 0.75f), vertical * (runSpeed * 0.75f));
+      } else
+      {
+        rb.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+      }
+    }
   }
 }
