27c2ac8d948d6d1fe5f5f011d0cd46d32707e624
merLO <ilia.reingold@gmail.com>
2020-11-09 20:11:47 -0500

    generate block func

---
+++ Assets/scripts/IslandGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class IslandGenerator : MonoBehaviour
+{
+  //public TileBase waterTile;
+  public TileBase grassTile;
+  public Tilemap tileMap;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    GenerateBlock();
+  }
+
+  // rules -> super cool
+  // massive dynamic width/height
+  // intersperse water riles
+
+  void GenerateBlock()
+  {
+    for (int x = 0; x < 10; x++)
+    {
+      for (int y = 0; y < 10; y++)
+      {
+        Debug.Log("setting tile");
+        tileMap.SetTile(new Vector3Int(x, y, 0), grassTile);
+      }
+    }
+  }
+}
